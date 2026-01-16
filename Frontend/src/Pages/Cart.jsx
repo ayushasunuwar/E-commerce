c9f30f328c2d381f4765a2d259690eea01068f9c
ayushasunuwar <aisharodriguez91@gmail.com>
2026-01-16 09:10:34 +0545

    Place order completed

--- Frontend/src/Pages/Cart.jsx
+++ Frontend/src/Pages/Cart.jsx
@@ -6,7 +6,7 @@ import CartTotal from '../Components/CartTotal';
 
 const Cart = () => {
 
-  const { products, currency, cartItems, updateQuantity } = useContext(ShopContext);
+  const { products, currency, cartItems, updateQuantity, navigate } = useContext(ShopContext);
 
   const [cartData, setCartData] = useState([]);
 
@@ -67,7 +67,7 @@ const Cart = () => {
           <CartTotal/>
           <div className='w-full text-end'>
 
-            <button className='bg-black text-white text-sm my-8 px-8 py-3'>PROCEED TO CHECKOUT</button>
+            <button onClick={() => navigate('/place-order')} className='bg-black text-white text-sm my-8 px-8 py-3'>PROCEED TO CHECKOUT</button>
 
           </div>
         </div>
