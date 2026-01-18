6c2e214512e7d412adfdf8c13f5318865e28a43e
ayushasunuwar <aisharodriguez91@gmail.com>
2026-01-18 14:13:47 +0545

    Created user model, user controller and product model

--- Backend/server.js
+++ Backend/server.js
@@ -3,6 +3,7 @@ import cors from 'cors'
 import 'dotenv/config'
 import connectDB from './config/mongodb.js'
 import connectCloudinary from './config/cloudinary.js'
+import userRouter from './routes/userRoute.js'
 
 //App config
 const app = express()
@@ -15,8 +16,6 @@ app.use(express.json())
 app.use(cors())
 
 //API endpoints
-app.get('/', (req, res) => {
-    res.send("API working")
-})
+app.use('/api/user', userRouter)
 
 app.listen(port, () => console.log('Server started on port: '+port))
