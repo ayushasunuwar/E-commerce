34eb0a46cf37b9b309c3fe5e043acaba97c82ae4
ayushasunuwar <aisharodriguez91@gmail.com>
2026-01-20 13:05:42 +0545

    Collection and Product connected to backend

--- Frontend/src/Context/ShopContext.jsx
+++ Frontend/src/Context/ShopContext.jsx
@@ -1,5 +1,4 @@
 import { createContext, useEffect, useState } from "react";
-import { products } from "../assets/front_end_assets/assets";
 import { toast } from "react-toastify";
 import { useNavigate } from "react-router-dom";
 
@@ -10,9 +9,12 @@ const ShopContextProvider = (props) => {
 
     const currency = '$';
     const delivery_fee = 10;
+    const backendUrl = import.meta.env.VITE_BACKEND_URL;
     const [search, setSearch] = useState('');
     const [showSearch, setShowSearch] = useState(false);
     const [cartItems, setCartItems] = useState({});
+    const [products, setProducts] = useState([])
+    const [token, setToken] = useState('')
     const navigate = useNavigate();
 
     const addToCart = async (itemId, size) => {
@@ -38,6 +40,14 @@ const ShopContextProvider = (props) => {
         }
 
         setCartItems(cartData);
+
+        if(token){
+            try {
+                
+            } catch (error) {
+                
+            }
+        }
     }
 
     const getCartCount = () => {
@@ -84,12 +94,35 @@ const ShopContextProvider = (props) => {
         return totalAmount;
     }
 
+    const getProductsData = async () => {
+        try {
+            
+            const response = await Axis3DIcon.get(backendUrl + '/api/products/list')
+            if (response.data.success) {
+                setProducts(response.data.products)
+            }
+            else{
+                toast.error(response.data.message)
+            }
+        } catch (error) {
+            console.log(error)
+            toast.error(error.message)
+        }
+    }
+
+    useEffect(() => {
+        if (!token && localStorage.getItem('token')) {
+            setToken(localStorage.getItem('token'))
+        }
+    }, [])
+
     const value = {
         products, currency, delivery_fee,
         search, setSearch, showSearch, setShowSearch,
         cartItems, addToCart,
         getCartCount, updateQuantity, getCartAmount,
-        navigate
+        navigate, backendUrl,
+        setToken, token
     }
 
     return(
