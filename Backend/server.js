a59567fcf30a2dc7910ad0d2a13c8432bbc350d0
ayushasunuwar <aisharodriguez91@gmail.com>
2026-01-20 13:43:46 +0545

    Added cart controller, model and router

--- Backend/server.js
+++ Backend/server.js
@@ -5,6 +5,7 @@ import connectDB from './config/mongodb.js'
 import connectCloudinary from './config/cloudinary.js'
 import userRouter from './routes/userRoute.js'
 import productRouter from './routes/productRoute.js'
+import cartRouter from './routes/cartRoute.js'
 
 //App config
 const app = express()
@@ -19,5 +20,6 @@ app.use(cors())
 //API endpoints
 app.use('/api/user', userRouter)
 app.use('/api/product', productRouter)
+app.use('/api/cart', cartRouter)
 
 app.listen(port, () => console.log('Server started on port: '+port))
